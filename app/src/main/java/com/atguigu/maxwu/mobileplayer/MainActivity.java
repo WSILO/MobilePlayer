49d1df88682e7c12a6e2670d2777e30ae5bba4d6
MaxWu <648838173@qq.com>
2017-05-28 19:14:00 +0800

    框架搭建完成

--- app/src/main/java/com/atguigu/maxwu/mobileplayer/MainActivity.java
+++ app/src/main/java/com/atguigu/maxwu/mobileplayer/MainActivity.java
@@ -1,13 +1,88 @@
 package com.atguigu.maxwu.mobileplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.atguigu.maxwu.mobileplayer.fragment.BaseFragment;
+import com.atguigu.maxwu.mobileplayer.fragment.LocalAudioFragment;
+import com.atguigu.maxwu.mobileplayer.fragment.LocalVideoFragment;
+import com.atguigu.maxwu.mobileplayer.fragment.NetAudioFragment;
+import com.atguigu.maxwu.mobileplayer.fragment.NetVideoFragment;
+import com.atguigu.maxwu.mobileplayer.fragment.RecyclerNetAudioFragment;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private RadioGroup radio;
+    private int position;
+    private BaseFragment tempFragment;
+    private ArrayList<BaseFragment> fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initData();
+        radio.setOnCheckedChangeListener(this);
+        radio.check(R.id.local_video);
+    }
+
+    private void initData() {
         setContentView(R.layout.activity_main);
+        radio = (RadioGroup)findViewById(R.id.radio);
+        fragments = new ArrayList<>();
+        addDataToList();
+    }
+
+    private void addDataToList() {
+        fragments.add(new LocalVideoFragment());
+        fragments.add(new LocalAudioFragment());
+        fragments.add(new NetVideoFragment());
+        fragments.add(new NetAudioFragment());
+        fragments.add(new RecyclerNetAudioFragment());
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.local_video:
+                position = 0;
+                break;
+            case R.id.local_audio:
+                position = 1;
+                break;
+            case R.id.net_video:
+                position = 2;
+                break;
+            case R.id.net_listview_audio:
+                position = 3;
+                break;
+            case R.id.net_recycler_audio:
+                position = 4;
+                break;
+        }
+        BaseFragment currentFragment = fragments.get(position);
+        addFragment(currentFragment);
+    }
+
+    private void addFragment(BaseFragment currentFragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if(tempFragment != currentFragment) {
+            if(!currentFragment.isAdded()) {
+                if(tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                ft.add(R.id.fl_content, currentFragment);
+            }else {
+                if(tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                ft.show(currentFragment);
+            }
+            tempFragment = currentFragment;
+        }
+        ft.commit();
     }
 }
---
+++ app/src/main/java/com/atguigu/maxwu/mobileplayer/fragment/NetAudioFragment.java
@@ -0,0 +1,31 @@
+package com.atguigu.maxwu.mobileplayer.fragment;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * 作者: WuKai
+ * 时间: 2017/5/28
+ * 邮箱: 648838173@qq.com
+ * 作用:
+ */
+
+public class NetAudioFragment extends BaseFragment {
+    private TextView textView;
+    @Override
+    public View initView() {
+        textView = new TextView(mContext);
+        textView.setTextSize(30);
+        textView.setTextColor(Color.BLUE);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("网络音频(ListView)");
+    }
+}
